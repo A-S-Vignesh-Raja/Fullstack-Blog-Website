649ba635f197e5df50c35a82a2c6b59265158e87
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-01-16 14:30:17 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import IndexPage from './pages/indexpage';
 import LoginPage from './pages/loginpage';
 import RegisterPage from './pages/registerpage';
 import { UserContextProvider } from './UserContext';
+import CreatePost from './pages/createpost';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route index element={<IndexPage />}/>
           <Route path='/login' element={<LoginPage />}/>
           <Route path='/register' element={<RegisterPage />}/>
+          <Route path='/create' element={<CreatePost/>}/>
         </Route>
         
       </Routes>
---
+++ client/src/pages/createpost.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css'
+
+export default function CreatePost(){
+    const [title,setTitle] = useState('');
+    const [summary,setSummary]= useState('');
+    const [content,setContent]= useState('');
+    return(
+        <form>
+            <input type="title" 
+                placeholder={'Title'} 
+                value={title} 
+                onChange={ev => setTitle(ev.target.value)}/>
+            <input type="summary" 
+                placeholder={'Summary'} 
+                value={summary}
+                onChange={ev => setSummary(ev.target.value)}/>
+            <input type="file" onChange={ev => setFiles(ev.target.files)} />
+            <ReactQuill value={content} />
+            <button style={{marginTop:"7px"}}>Create Post</button>
+        </form>
+    );
+}
