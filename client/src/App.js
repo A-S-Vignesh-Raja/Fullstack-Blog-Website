bb7b5b4a514cf0f8d5038ddef0b4c3058f73abc8
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-01-21 21:31:43 +0530

    changed

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ function App() {
           <Route path='/login' element={<LoginPage />}/>
           <Route path='/register' element={<RegisterPage />}/>
           <Route path='/create' element={<CreatePost/>}/>
+          
         </Route>
         
       </Routes>
