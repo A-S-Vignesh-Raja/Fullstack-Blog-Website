acc9b86b60a13620e3ad3f5d66ce9646e272cb70
A.S.Vignesh Raja <136581635+A-S-Vignesh-Raja@users.noreply.github.com>
2024-01-23 20:49:50 +0530

    Update user.js

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const {Schema,model}=mongoose;
 
+
 const UserSchema= new Schema({
     username:{type:String,required:true,min:4,unique:true},
     password:{type:String,required:true},
