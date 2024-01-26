42413d8e3e2a99eb2c11557a842d6cbc345aaf75
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-01-26 21:37:31 +0530

    updated

--- client/src/pages/indexpage.js
+++ client/src/pages/indexpage.js
@@ -1,6 +1,14 @@
 import Post from "../post";
+import { useEffect } from "react";
 
 export default function IndexPage(){
+    useEffect(()=>{
+        fetch('/post').then(response =>{
+            response.json().then(posts =>{
+                console.log(posts);
+            });
+        });
+    }, []);
     return(
         <>
             <Post />
