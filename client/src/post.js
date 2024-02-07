9b1692c95953bc5b980f04b2275d5a094644a742
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-02-07 21:51:35 +0530

    updated

--- client/src/post.js
+++ client/src/post.js
@@ -12,6 +12,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
           <Link to={`/post/${_id}`}>
             <h2>{title}</h2>
           </Link> 
+          
           <p className="info">
             <a className="author">{author.username}</a>
             <time>{format(new Date(createdAt),'MMM d, yyyy HH:mm')}</time>
